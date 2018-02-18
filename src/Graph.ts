c6898611a2910aa0f17891888b82a36ae2a1e18a
Alexander Peters <rainesabaal@gmail.com>
2018-02-18 13:14:12 -0700

    Initial commit

---
+++ src/Graph.ts
@@ -0,0 +1,175 @@
+import { hash, Record, Map, ValueObject, Seq } from "immutable";
+
+type NodeKey = string;
+
+class EdgeKey implements ValueObject {
+  constructor(public source: NodeKey, public target: NodeKey) {}
+
+  equals(other: EdgeKey): boolean {
+    return this.source === other.source && this.target === other.target;
+  }
+
+  hashCode(): number {
+    let result = 17;
+    result = result * 37 + hash(this.source);
+    result = result * 37 + hash(this.target);
+    return result;
+  }
+}
+
+interface GraphProps<N, E> {
+  nodes: Map<NodeKey, N>;
+  edges: Map<EdgeKey, E>;
+}
+
+interface GraphMethods<N, E> {
+  predecessors(key: NodeKey): Seq<NodeKey, N>;
+  successors(key: NodeKey): Seq<NodeKey, N>;
+  neighbors(key: NodeKey): Seq<NodeKey, N>;
+
+  hasNode(key: NodeKey): boolean;
+  getNode<NSV>(key: NodeKey, notSetValue: NSV): N | NSV;
+  getNode(key: NodeKey): N | undefined;
+  setNode(key: NodeKey, node: N): this;
+  deleteNode(key: NodeKey): this;
+
+  hasEdge(srcKey: NodeKey, tgtKey: NodeKey): boolean;
+  getEdge<NSV>(srcKey: NodeKey, tgtKey: NodeKey, notSetValue: NSV): E | NSV;
+  getEdge(srcKey: NodeKey, tgtKey: NodeKey): E | undefined;
+  connect(srcKey: NodeKey, tgtKey: NodeKey, edge: E): this;
+  disconnect(srcKey: NodeKey, tgtKey: NodeKey): this;
+
+  findSources(): Seq<NodeKey, N>;
+  findSinks(): Seq<NodeKey, N>;
+  topoSort(): Seq.Indexed<[NodeKey, N]> | undefined;
+  hasCycles(): boolean;
+}
+
+class UntypedGraph<N, E> extends Record<GraphProps<any, any>>({
+  nodes: Map(),
+  edges: Map(),
+}) implements GraphMethods<N, E> {
+  predecessors(key: NodeKey): Seq<NodeKey, N> {
+    return Seq(this.edges)
+      .filter((_, k) => k.target === key)
+      .mapEntries(([k, _]) => [k.source, this.nodes.get(k.source)]);
+  }
+  successors(key: NodeKey): Seq<NodeKey, N> {
+    return Seq(this.edges)
+      .filter((_, k) => k.source === key)
+      .mapEntries(([k, _]) => [k.target, this.nodes.get(k.target)]);
+  }
+  neighbors(key: NodeKey): Seq<NodeKey, N> {
+    const pre = this.predecessors(key);
+    const suc = this.successors(key).filterNot((_, k) => pre.has(k));
+    return pre.concat(suc) as Seq<NodeKey, N>;
+  }
+
+  hasNode(key: NodeKey): boolean {
+    return this.nodes.has(key);
+  }
+  getNode<NSV>(key: NodeKey, notSetValue?: NSV): N | NSV | undefined {
+    return this.nodes.get(key, notSetValue);
+  }
+  setNode(key: NodeKey, node: N): this {
+    return this.setIn(["nodes", key], node);
+  }
+  deleteNode(key: NodeKey): this {
+    return this.deleteIn(["nodes", key]);
+  }
+
+  hasEdge(srcKey: NodeKey, tgtKey: NodeKey): boolean {
+    return this.edges.has(new EdgeKey(srcKey, tgtKey));
+  }
+  getEdge<NSV>(
+    srcKey: NodeKey,
+    tgtKey: NodeKey,
+    notSetValue?: NSV,
+  ): E | NSV | undefined {
+    return this.edges.get(new EdgeKey(srcKey, tgtKey), notSetValue);
+  }
+  connect(srcKey: NodeKey, tgtKey: NodeKey, edge: E): this {
+    if (!this.hasNode(srcKey) || !this.hasNode(tgtKey)) {
+      return this;
+    }
+    return this.setIn(["edges", new EdgeKey(srcKey, tgtKey)], edge);
+  }
+  disconnect(srcKey: NodeKey, tgtKey: NodeKey): this {
+    if (!this.hasNode(srcKey) || !this.hasNode(tgtKey)) {
+      return this;
+    }
+    return this.deleteIn(["edges", new EdgeKey(srcKey, tgtKey)]);
+  }
+
+  findSources(): Seq<NodeKey, N> {
+    const targets = this.edges.keySeq().map(({ target }) => target);
+    return this.nodes.toSeq().filterNot((_, k) => targets.contains(k));
+  }
+  findSinks(): Seq<NodeKey, N> {
+    const sources = this.edges.keySeq().map(({ source }) => source);
+    return this.nodes.toSeq().filterNot((_, k) => sources.contains(k));
+  }
+  topoSort(): Seq.Indexed<[NodeKey, N]> {
+    let wavefront = this.findSources()
+      .keySeq()
+      .toSet();
+    let edges = this.edges;
+    return Seq({
+      [Symbol.iterator]: () => {
+        return {
+          next: () => {
+            const nodeKey = wavefront.first();
+            if (!nodeKey) {
+              return { done: true, value: (undefined as any) as [NodeKey, N] };
+            }
+            wavefront = wavefront.skip(1);
+            edges = edges.filter((_, { source }) => source !== nodeKey);
+            this.successors(nodeKey)
+              .filterNot((_, sucKey) =>
+                edges.some((_, edgeKey) => edgeKey.target === sucKey),
+              )
+              .forEach((_, sucKey) => (wavefront = wavefront.add(sucKey)));
+
+            return {
+              done: false,
+              value: [nodeKey, this.getNode(nodeKey)! as N],
+            };
+          },
+        };
+      },
+    } as Iterable<[NodeKey, N]>);
+  }
+  hasCycles(): boolean {
+    return this.topoSort().count() !== this.nodes.count();
+  }
+}
+
+type Graph<N, E = N> = UntypedGraph<N, E> &
+  Readonly<GraphProps<N, E>> &
+  GraphMethods<N, E>;
+
+function Graph<N, E = N>(values?: Partial<GraphProps<N, E>>): Graph<N, E> {
+  return new UntypedGraph(values);
+}
+
+namespace Graph {
+  export function from<N, E>(
+    nodes: [NodeKey, N][],
+    edges?: [NodeKey, NodeKey, N][],
+  ): Graph<N, E> {
+    return new UntypedGraph({
+      nodes: Map(nodes),
+      edges: edges
+        ? Seq(edges)
+            .toKeyedSeq()
+            .mapEntries(([_, [source, target, node]]) => [
+              new EdgeKey(source, target),
+              node,
+            ])
+            .toMap()
+        : Map(),
+    });
+  }
+}
+
+export { Graph };
---
+++ src/__tests__/Graph.ts
@@ -0,0 +1,275 @@
+import { Seq } from "immutable";
+
+import { Graph } from "../Graph";
+
+const NodeSeq = (nodes: string[]) =>
+  Seq(nodes)
+    .toKeyedSeq()
+    .mapKeys((_, v) => v.toUpperCase());
+
+const undefKey = (undefined as any) as string;
+
+const nodes: [string, string][] = [
+  ["H", "h"],
+  ["G", "g"],
+  ["F", "f"],
+  ["E", "e"],
+  ["D", "d"],
+  ["C", "c"],
+  ["B", "b"],
+  ["A", "a"],
+];
+const edges: [string, string, string][] = [
+  ["F", "H", "fh"],
+  ["F", "G", "fg"],
+  ["E", "F", "ef"],
+  ["D", "F", "df"],
+  ["C", "D", "cd"],
+  ["B", "D", "bd"],
+  ["A", "C", "ac"],
+  ["A", "B", "ab"],
+];
+const graph = Graph.from(nodes, edges);
+
+describe("Graph structure", () => {
+  describe("initializers", () => {
+    describe("from()", () => {
+      it("returns a Graph containing all passed nodes and edges", () => {
+        expect(graph.hasNode("A")).toBeTruthy();
+        expect(graph.hasNode("B")).toBeTruthy();
+        expect(graph.hasNode("C")).toBeTruthy();
+        expect(graph.hasNode("D")).toBeTruthy();
+        expect(graph.hasNode("E")).toBeTruthy();
+        expect(graph.hasNode("F")).toBeTruthy();
+        expect(graph.hasNode("G")).toBeTruthy();
+        expect(graph.hasNode("H")).toBeTruthy();
+
+        expect(graph.hasEdge("A", "B")).toBeTruthy();
+        expect(graph.hasEdge("A", "C")).toBeTruthy();
+        expect(graph.hasEdge("B", "D")).toBeTruthy();
+        expect(graph.hasEdge("C", "D")).toBeTruthy();
+        expect(graph.hasEdge("D", "F")).toBeTruthy();
+        expect(graph.hasEdge("E", "F")).toBeTruthy();
+        expect(graph.hasEdge("F", "G")).toBeTruthy();
+        expect(graph.hasEdge("F", "H")).toBeTruthy();
+      });
+    });
+  });
+
+  describe("instance methods", () => {
+    describe("predecessors()", () => {
+      const pre = NodeSeq(["d", "e"]);
+
+      it("returns all nodes with an edge ending at the passed node", () => {
+        expect(graph.predecessors("F").equals(pre)).toBeTruthy();
+      });
+    });
+
+    describe("successors()", () => {
+      const suc = NodeSeq(["g", "h"]);
+
+      it("returns all nodes with an edge starting at the passed node", () => {
+        expect(graph.successors("F").equals(suc)).toBeTruthy();
+      });
+    });
+
+    describe("neighbors()", () => {
+      const nbr = NodeSeq(["d", "e", "g", "h"]);
+
+      it("returns all of the passed node's neighbors", () => {
+        expect(graph.neighbors("F").equals(nbr)).toBeTruthy();
+      });
+
+      it("doesn't return any neighbors more than once", () => {
+        expect(
+          graph
+            .connect("H", "F", "hf")
+            .neighbors("F")
+            .count(),
+        ).toBe(4);
+      });
+    });
+
+    describe("hasNode()", () => {
+      it("returns true for node keys that are present", () => {
+        expect(graph.hasNode("A")).toBeTruthy();
+        expect(graph.hasNode("H")).toBeTruthy();
+      });
+
+      it("returns false for node keys that are absent", () => {
+        expect(graph.hasNode("Z")).toBeFalsy();
+        expect(graph.hasNode(undefKey)).toBeFalsy();
+      });
+    });
+
+    describe("getNode()", () => {
+      describe("when there is a node at the given key", () => {
+        it("returns the value assigned to the given node key", () => {
+          expect(graph.getNode("A")).toBe("a");
+          expect(graph.getNode("H")).toBe("h");
+        });
+
+        it("ignores the passed notSetValue", () => {
+          expect(graph.getNode("A", "not set")).toBe("a");
+        });
+      });
+
+      describe("when there is no node at the given key", () => {
+        it("returns the notSetValue passed", () => {
+          expect(graph.getNode("Z", "not set")).toBe("not set");
+          expect(graph.getNode(undefKey, "unset")).toBe("unset");
+        });
+
+        it("returns undefined if there is no notSetValue", () => {
+          expect(graph.getNode("Z")).toBeUndefined();
+          expect(graph.getNode(undefKey, "unset")).toBe("unset");
+        });
+      });
+    });
+
+    describe("setNode()", () => {
+      it("returns a new Graph", () => {
+        expect(graph.setNode("I", "i")).not.toBe(graph);
+      });
+
+      it("adds the passed node to the new Graph", () => {
+        expect(graph.setNode("I", "i").hasNode("I")).toBeTruthy();
+      });
+    });
+
+    describe("deleteNode()", () => {
+      it("returns a new Graph", () => {
+        expect(graph.deleteNode("A")).not.toBe(graph);
+      });
+
+      it("removes the node at the passed index from the new Graph", () => {
+        expect(graph.deleteNode("A").hasNode("A")).toBeFalsy();
+      });
+    });
+
+    describe("hasEdge()", () => {
+      it("returns true for connections that exist", () => {
+        expect(graph.hasEdge("A", "B")).toBeTruthy();
+        expect(graph.hasEdge("F", "H")).toBeTruthy();
+      });
+
+      it("returns false for connections that do not exist", () => {
+        expect(graph.hasEdge("A", "F")).toBeFalsy();
+        expect(graph.hasEdge("A", undefKey)).toBeFalsy();
+      });
+    });
+
+    describe("getEdge()", () => {
+      describe("when there is an edge between the given keys", () => {
+        it("returns the value assigned to the edge between the given keys", () => {
+          expect(graph.getEdge("A", "B")).toBe("ab");
+        });
+
+        it("ignores the passed notSetValue", () => {
+          expect(graph.getEdge("A", "B", "not set")).toBe("ab");
+        });
+      });
+
+      describe("when there is no edge between the given keys", () => {
+        it("returns the notSetValue passed", () => {
+          expect(graph.getEdge("C", "E", "not set")).toBe("not set");
+          expect(graph.getEdge(undefKey, undefKey, "unset")).toBe("unset");
+        });
+
+        it("returns undefined if there is no notSetValue", () => {
+          expect(graph.getEdge("C", "E")).toBeUndefined();
+          expect(graph.getEdge(undefKey, undefKey)).toBeUndefined();
+        });
+      });
+    });
+
+    describe("connect()", () => {
+      it("returns a new Graph", () => {
+        expect(graph.connect("A", "E", "ae")).not.toBe(graph);
+      });
+
+      it("adds the given edge to the new Graph", () => {
+        expect(graph.connect("A", "E", "ae").getEdge("A", "E")).toBe("ae");
+      });
+
+      describe("when an invalid key is passed", () => {
+        it("returns the same Graph", () => {
+          expect(graph.connect("A", "Z", "az")).toBe(graph);
+          expect(graph.connect(undefKey, undefKey, "invalid")).toBe(graph);
+        });
+      });
+    });
+
+    describe("disconnect()", () => {
+      it("returns a new Graph", () => {
+        expect(graph.disconnect("A", "B")).not.toBe(graph);
+      });
+
+      it("removes the given edge from the new Graph", () => {
+        expect(graph.disconnect("A", "B").getEdge("A", "B")).toBeUndefined();
+      });
+
+      describe("when an invalid key is passed", () => {
+        it("returns the same Graph", () => {
+          expect(graph.disconnect("A", "Z")).toBe(graph);
+          expect(graph.disconnect(undefKey, undefKey)).toBe(graph);
+        });
+      });
+    });
+
+    describe("findSources()", () => {
+      const sources = NodeSeq(["a", "e"]);
+
+      it("returns all nodes that aren't edge targets", () => {
+        expect(graph.findSources().equals(sources)).toBeTruthy();
+      });
+    });
+
+    describe("findSinks()", () => {
+      const sinks = NodeSeq(["g", "h"]);
+
+      it("returns all nodes that aren't edge sources", () => {
+        expect(graph.findSinks().equals(sinks)).toBeTruthy();
+      });
+    });
+
+    describe("topoSort()", () => {
+      // Branching nodes excluded to avoid order ambiguity
+      const sorted = Seq.Indexed(["A", "B", "D", "F", "G"]);
+
+      it("returns nodes sorted topologically", () => {
+        expect(
+          graph.topoSort()!
+            .filter(([k, _]) => sorted.contains(k))
+            .map(([k, _]) => k)
+            .equals(sorted),
+        ).toBeTruthy();
+      });
+
+      it("returns the same number of nodes as in the graph", () => {
+        expect(graph.topoSort().count()).toBe(graph.nodes.count());
+      });
+
+      describe("when a cycle exists", () => {
+        it("returns fewer than all nodes", () => {
+          expect(
+            graph
+              .connect("G", "A", "ga")
+              .topoSort()
+              .count(),
+          ).toBeLessThan(graph.nodes.count());
+        });
+      });
+    });
+
+    describe("hasCycles()", () => {
+      it("returns false if there is no cycle", () => {
+        expect(graph.hasCycles()).toBeFalsy();
+      });
+
+      it("returns true if there is a cycle", () => {
+        expect(graph.connect("F", "A", "fa").hasCycles()).toBeTruthy();
+      });
+    });
+  });
+});
